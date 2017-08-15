9d1b62724a9e2f19d6c820aa24a47d327121e14c
MrMPerial <christopher.mitchell26@gmail.com>
2017-08-14 20:30:06 -0500

    Created New Server

--- emojis.js
+++ emojis.js
@@ -1,8 +1,23 @@
+const http = require('http');
 const emoji = require('node-emoji');
 
+const server = http.createServer((req, res) => {
+  res.write(myEmoji);
+  res.end();
+});
+
+server.listen(8001);
+
+let myEmoji = emoji.random().emoji;
+
+let emojiArray = [];
 function listEmojis() {
   for (let i = 0; i < 10; i++) {
-    console.log(emoji.random().emoji);
+    emojiArray.push(emoji.random().emoji);
+  }
+
+  for (let i = 0; i < emojiArray.length; i++) {
+    console.log(emojiArray[i]);
   }
 }
 
